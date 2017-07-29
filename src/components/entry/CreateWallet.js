cd012fed0750f3b76bd8beadc9098a837224cd57
ddabek <xordaniel@gmail.com>
2017-07-30 00:39:23 +0200

    enc, dec, create wallet

--- src/components/entry/CreateWallet.js
+++ src/components/entry/CreateWallet.js
@@ -2,6 +2,8 @@ import React from 'react';
 import crypto from 'crypto';
 var fs = window.require('fs');
 var os = window.require('os');
+var bs58 = require('bs58');
+var bitcoin = window.require('bitcoinjs-lib');
 
 
 
@@ -9,29 +11,116 @@ export default class CreateWallet extends React.Component {
     constructor(props) {
         super(props);
 
+        this.state = {
+            password1: '',
+            password2: '',
+            walletExists: false,
+        }
+        this.handleSubmit = this.handleSubmit.bind(this);
 
     }
 
     //here we create the wallet file in the default location after prompting for a password and creating the encrypted file.
 
-    create_wallet() {
+
+
+    handleSubmit(e) {
+        if (e.target.password1.value === e.target.password2.value) {
+            var random_array = new Uint8Array(32);
+            window.crypto.getRandomValues(random_array);
+            var priv_key_bytes = [];
+
+            for (var i = 0; i < random_array.length; ++i) {
+                priv_key_bytes[i] = random_array[i];
+            }
+
+            var hex_string = this.toHexString(priv_key_bytes).toUpperCase();
+
+            var priv_key_and_version = "80" + hex_string;
+            var first_bytes = Buffer.from(priv_key_and_version, 'hex');
+            var first_sha = bitcoin.crypto.sha256(first_bytes);
+            var second_sha = bitcoin.crypto.sha256(first_sha);
+            var checksum = this.toHexString(second_sha).substr(0, 8).toUpperCase();
+            var key_with_checksum = priv_key_and_version + checksum;
+
+            var final_bytes = Buffer.from(key_with_checksum, 'hex');
+            var priv_key_wif = bs58.encode(final_bytes);
+
+            var key_pair = bitcoin.ECPair.fromWIF(priv_key_wif);
+
+            var address = key_pair.getAddress();
+
+
+            var key_json = {};
+            key_json["public_key"] = address;
+            key_json["private_key"] = priv_key_wif;
+
+            var key_array = [];
+            key_array.push(key_json);
+
+            var json = {};
+
+            json["version"] = "1";
+            json["keys"] = key_array;
+
+            console.log(json);
+
+
+            var crypto = require('crypto'),
+                algorithm = 'aes-256-ctr',
+                password = e.target.password1.value;
+
+            var cipher_text = this.encrypt(JSON.stringify(json), algorithm, password);
+
+            console.log(cipher_text);
+
+            var decrypt_text = this.decrypt(cipher_text, algorithm, "a");
+
+            console.log(decrypt_text);
+
+            this.create_wallet(cipher_text);
+
+        }
+    }
+
+    create_wallet(encrypted_wallet_info) {
         var home_dir = os.homedir();
 
-        fs.writeFile(home_dir + '/safexwallet.dat', '', function (err) {
+        fs.writeFile(home_dir + '/safexwallet.dat', encrypted_wallet_info, function (err) {
             if (err) {
                 console.log(err);
             }
         });
     }
 
+    encrypt(text, algorithm, password) {
+        var cipher = crypto.createCipher(algorithm,password)
+        var crypted = cipher.update(text,'utf8','hex')
+        crypted += cipher.final('hex');
+        return crypted;
+    }
+
+    decrypt(text, algorithm, password){
+        var decipher = crypto.createDecipher(algorithm,password)
+        var dec = decipher.update(text,'hex','utf8')
+        dec += decipher.final('utf8');
+        return dec;
+    }
+
+    toHexString(byteArray) {
+        return Array.from(byteArray, function(byte) {
+            return ('0' + (byte & 0xFF).toString(16)).slice(-2);
+        }).join('')
+    }
+
 
     render() {
         return (
             <div>
-                <form>
-                    <input type="password" name="first_password" />
-                    <input type="password" name="secon_password" />
-
+                <form onSubmit={this.handleSubmit}>
+                    <input type="password" name="password1" />
+                    <input type="password" name="password2" />
+                    <button type="submit">Save Wallet</button>
 
                 </form>
             </div>
