80cc5a5a99cec7e1ad88a88d1b5bd8a0db8a04ec
ddabek <xordaniel@gmail.com>
2017-08-24 15:39:41 +0200

    todo prevent 0 balance transactions

--- src/components/wallet/Wallet.js
+++ src/components/wallet/Wallet.js
@@ -191,7 +191,7 @@ export default class Wallet extends React.Component {
         });
     }
 
-
+	//todo prevent 0 balance safex/bitcoin transactions
     sendCoins(e) {
         e.preventDefault();
         this.setState({
