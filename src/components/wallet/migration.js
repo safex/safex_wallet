0bec418b962b2308a2e21fb9d566a569215dbe7e
Daniel Dabek <xordaniel@gmail.com>
2018-08-17 01:04:57 +0200

    migration

---
+++ src/components/wallet/migration.js
@@ -0,0 +1,42 @@
+
+
+
+function generateSafexKeys() {
+
+}
+
+
+
+
+
+//standard BTC Blockchain Safex transaction
+function generateSafexBtcTransaction() {
+
+}
+
+
+//set the public key to where you want safex tokens sent to and safex cash
+function setSafexMigrationPublicKey() {
+
+}
+
+function generateSafexTokenTransaction() {
+
+}
+
+
+
+function generateSafexCashTransaction() {
+
+}
+
+//this should only be set if the public address has been set
+function generateSafexMigrationTransaction() {
+
+}
+
+
+
+module.exports = {
+
+};
