06e76f505dc7093d7acdda7f0903087093c7aa28
Uros <umitrovic1@gmail.com>
2018-11-02 14:39:06 +0100

    add confirm migration modal

--- src/components/migration/Migrate4.js
+++ src/components/migration/Migrate4.js
@@ -1,176 +1,207 @@
-import React from 'react';
+import React from "react";
 
-import {get_utxos, broadcastTransaction, setSafexMigrationAddress, BURN_ADDRESS, getFee} from '../../utils/migration';
-import {openMigrationAlert, closeMigrationAlert, openResetMigration, closeResetMigration, confirmReset} from '../../utils/modals';
+import {
+  get_utxos,
+  broadcastTransaction,
+  setSafexMigrationAddress,
+  BURN_ADDRESS,
+  getFee
+} from "../../utils/migration";
+import {
+  openMigrationAlert,
+  closeMigrationAlert,
+  openResetMigration,
+  closeResetMigration,
+  confirmReset
+} from "../../utils/modals";
 import MigrationAlert from "../partials/MigrationAlert";
-import ResetMigration from "../partials/ResetMigration";
 
 //Set Second Half of the Safex Address
 export default class Migrate4 extends React.Component {
-    constructor(props) {
-        super(props);
-        this.state = {
-            loading: true,
-            address: "",
-            wif: "",
-            safex_bal: 0,
-            pending_safex_bal: 0,
-            btc_bal: 0,
-            pending_btc_bal: 0,
-            safex_price: 0,
-            btc_price: 0,
-            status_text: "",
-            safex_key: {},
-            migration_alert: false,
-            migration_alert_text: '',
-            fee: 0
-        };
+  constructor(props) {
+    super(props);
+    this.state = {
+      loading: true,
+      address: "",
+      wif: "",
+      safex_bal: 0,
+      pending_safex_bal: 0,
+      btc_bal: 0,
+      pending_btc_bal: 0,
+      safex_price: 0,
+      btc_price: 0,
+      status_text: "",
+      safex_key: {},
+      migration_alert: false,
+      migration_alert_text: "",
+      fee: 0
+    };
 
-        this.refresh = this.refresh.bind(this);
-        this.setSafexAddress = this.setSafexAddress.bind(this);
-        this.setOpenMigrationAlert = this.setOpenMigrationAlert.bind(this);
-        this.setCloseMigrationAlert = this.setCloseMigrationAlert.bind(this);
-    }
+    this.refresh = this.refresh.bind(this);
+    this.setSafexAddress = this.setSafexAddress.bind(this);
+    this.setOpenMigrationAlert = this.setOpenMigrationAlert.bind(this);
+    this.setCloseMigrationAlert = this.setCloseMigrationAlert.bind(this);
+  }
 
-    getTxnFee() {
-        console.log(this.state.safex_key);
-        //public view key is second half
-        get_utxos(this.props.data.address)
-            .then(utxos => {
-                const payload = '536166657832' +
-                    this.props.data.safex_key.view.pub +
-                    this.props.data.safex_key.checksum;
+  getTxnFee() {
+    console.log(this.state.safex_key);
+    //public view key is second half
+    get_utxos(this.props.data.address)
+      .then(utxos => {
+        const payload =
+          "536166657832" +
+          this.props.data.safex_key.view.pub +
+          this.props.data.safex_key.checksum;
 
-                const rawtx = setSafexMigrationAddress(
-                    utxos,
-                    BURN_ADDRESS,
-                    this.state.wif,
-                    payload,
-                    this.props.data.fee
-                );
+        const rawtx = setSafexMigrationAddress(
+          utxos,
+          BURN_ADDRESS,
+          this.state.wif,
+          payload,
+          this.props.data.fee
+        );
 
-                this.setState({txn_fee: rawtx.fee / 100000000});
-            })
-            .catch(err => {
-                console.log("error getting UTXOs " + err);
-                this.setOpenMigrationAlert("error getting UTXOs " + err);
-            });
-    }
+        this.setState({ txn_fee: rawtx.fee / 100000000 });
+      })
+      .catch(err => {
+        console.log("error getting UTXOs " + err);
+        this.setOpenMigrationAlert("error getting UTXOs " + err);
+      });
+  }
 
-    componentDidMount() {
+  componentDidMount() {
+    this.setState({
+      address: this.props.data.address,
+      wif: this.props.data.wif,
+      safex_key: this.props.data.safex_key,
+      fee: this.props.data.fee
+    });
+
+    this.getBalances(this.props.data.address);
+    this.getTxnFee();
+    this.setState({ loading: false });
+  }
+
+  getBalances(address) {
+    var promises = [];
+    var json = {};
+    json["address"] = address;
+    promises.push(
+      fetch(
+        "http://bitcoin.safex.io:3001/insight-api/addr/" + address + "/balance"
+      )
+        .then(resp => resp.text())
+        .then(resp => {
+          return resp;
+        })
+    );
+
+    Promise.all(promises)
+      .then(values => {
         this.setState({
-            address: this.props.data.address,
-            wif: this.props.data.wif,
-            safex_key: this.props.data.safex_key,
-            fee: this.props.data.fee,
+          btc_bal: (values[0] / 100000000).toFixed(8),
+          safex_price: localStorage.getItem("safex_price"),
+          btc_price: localStorage.getItem("btc_price")
         });
-
-        this.getBalances(this.props.data.address);
-        this.getTxnFee();
-        this.setState({loading: false});
-    }
-
-    getBalances(address) {
-        var promises = [];
-        var json = {};
-        json['address'] = address;
-        promises.push(fetch('http://bitcoin.safex.io:3001/insight-api/addr/' + address + '/balance')
-            .then(resp => resp.text())
-            .then((resp) => {
-                return resp
-            }));
-
-        Promise.all(promises).then(values => {
-            this.setState({
-                btc_bal: (values[0] / 100000000).toFixed(8),
-                safex_price: localStorage.getItem('safex_price'),
-                btc_price: localStorage.getItem('btc_price'),
-            });
-        }).catch(e => {
-            console.log(e)
-            this.setState({
-                status_text: 'Sync error, please refresh'
-            });
+      })
+      .catch(e => {
+        console.log(e);
+        this.setState({
+          status_text: "Sync error, please refresh"
         });
-    }
+      });
+  }
 
-    refresh(e) {
-        e.preventDefault();
-        this.getBalances(this.state.address);
-    }
+  refresh(e) {
+    e.preventDefault();
+    this.getBalances(this.state.address);
+  }
 
-    setSafexAddress(e) {
-        e.preventDefault();
-        console.log(this.state.safex_key)
-        //public spend key is first half
-        get_utxos(this.state.address)
-            .then(utxos => {
-                const payload = '536166657832' +
-                    this.props.data.safex_key.view.pub +
-                    this.props.data.safex_key.checksum;
+  setSafexAddress(e) {
+    e.preventDefault();
+    console.log(this.state.safex_key);
+    //public spend key is first half
+    get_utxos(this.state.address)
+      .then(utxos => {
+        const payload =
+          "536166657832" +
+          this.props.data.safex_key.view.pub +
+          this.props.data.safex_key.checksum;
 
-                const rawtx = setSafexMigrationAddress(
-                    utxos,
-                    BURN_ADDRESS,
-                    this.state.wif,
-                    payload,
-                    this.props.data.fee
-                );
+        const rawtx = setSafexMigrationAddress(
+          utxos,
+          BURN_ADDRESS,
+          this.state.wif,
+          payload,
+          this.props.data.fee
+        );
 
-                return rawtx;
+        return rawtx;
+      })
+      .then(rawtx => broadcastTransaction(rawtx))
+      .then(result => {
+        this.props.setMigrationProgress(4);
+        console.log(result);
+      })
+      .catch(err => {
+        console.log("error broadcasting transaction " + err);
+        this.setOpenMigrationAlert("error broadcasting transaction " + err);
+      })
+      .catch(err => {
+        console.log("error getting UTXOs " + err);
+        this.setOpenMigrationAlert("error getting UTXOs " + err);
+      });
+  }
 
-            })
-            .then(rawtx => broadcastTransaction(rawtx))
-            .then(result => {
-                this.props.setMigrationProgress(4);
-                console.log(result);
-            })
-            .catch(err => {
-                console.log("error broadcasting transaction " + err);
-                this.setOpenMigrationAlert("error broadcasting transaction " + err);
-            })
-            .catch(err => {
-                console.log("error getting UTXOs " + err);
-                this.setOpenMigrationAlert("error getting UTXOs " + err);
-            });
-    }
+  setOpenMigrationAlert(message) {
+    openMigrationAlert(this, message);
+  }
 
-    setOpenMigrationAlert(message) {
-        openMigrationAlert(this, message);
-    }
+  setCloseMigrationAlert() {
+    closeMigrationAlert(this);
+  }
 
-    setCloseMigrationAlert() {
-        closeMigrationAlert(this);
-    }
+  //take second half and send transaction
+  render() {
+    return (
+      <div>
+        <p>Step 3/4</p>
 
-    //take second half and send transaction
-    render() {
-        return (
-            <div>
-                <p>Step 3/4</p>
+        <p>
+          Setting your Safex Address for Migration requires two steps. In this
+          step we will set the Second Half of the Safex Address. This will
+          require a bitcoin fee. The next step will also require a bitcoin fee.
+        </p>
 
-                <p>
-                    Setting your Safex Address for Migration requires two steps. In this step we will set
-                    the Second Half of the Safex Address. This will require a bitcoin fee.
-                    The next step will also require a bitcoin fee.
-                </p>
+        <p>
+          <span className="span-200">Your target migration address:</span>
+          <br />
+          <input
+            className="target-address"
+            value={this.state.safex_key.public_addr}
+          />
+        </p>
 
-                <p>
-                    <span className="span-200">Your target migration address:</span><br />
-                    <input className="target-address" value={this.state.safex_key.public_addr} />
-                </p>
+        <p>
+          <span>You will need</span> {this.state.txn_fee} btc{" "}
+        </p>
+        <p>
+          <span>Your btc balance</span> {this.state.btc_bal} btc
+        </p>
 
-                <p><span>You will need</span> {this.state.txn_fee} btc </p>
-                <p><span>Your btc balance</span> {this.state.btc_bal} btc</p>
+        <button
+          className="button-shine green-btn"
+          onClick={this.setSafexAddress}
+        >
+          Set the second half
+        </button>
 
-                <button className="button-shine green-btn" onClick={this.setSafexAddress}>Set the second half</button>
-
-                <MigrationAlert
-                    migrationAlert={this.state.migration_alert}
-                    migrationAlertText={this.state.migration_alert_text}
-                    closeMigrationAlert={this.setCloseMigrationAlert}
-                />
-            </div>
-        )
-    }
+        <MigrationAlert
+          migrationAlert={this.state.migration_alert}
+          migrationAlertText={this.state.migration_alert_text}
+          closeMigrationAlert={this.setCloseMigrationAlert}
+        />
+      </div>
+    );
+  }
 }
--- src/components/migration/Migrate5.js
+++ src/components/migration/Migrate5.js
@@ -1,207 +1,266 @@
-import React from 'react';
+import React from "react";
 
 import {
-    get_utxos,
-    generateSafexBtcTransaction,
-    broadcastTransaction,
-    BURN_ADDRESS,
-    setSafexMigrationAddress,
-    getFee
-} from '../../utils/migration';
+  get_utxos,
+  generateSafexBtcTransaction,
+  broadcastTransaction,
+  BURN_ADDRESS,
+  setSafexMigrationAddress,
+  getFee
+} from "../../utils/migration";
 
-import {openMigrationAlert, closeMigrationAlert, openResetMigration, closeResetMigration, confirmReset} from '../../utils/modals';
+import {
+  openMigrationAlert,
+  closeMigrationAlert,
+  openResetMigration,
+  closeResetMigration,
+  confirmReset
+} from "../../utils/modals";
+
 import MigrationAlert from "../partials/MigrationAlert";
-import ResetMigration from "../partials/ResetMigration";
+import ConfirmMigration from "../partials/ConfirmMigration";
 
 //Burn Safe Exchange Coins
 export default class Migrate5 extends React.Component {
-    constructor(props) {
-        super(props);
-        this.state = {
-            safex_key: this.props.data.safex_key,
-            loading: true,
-            address: "",
-            wif: "",
-            safex_bal: 0,
-            pending_safex_bal: 0,
-            btc_bal: 0,
-            pending_btc_bal: 0,
-            safex_price: 0,
-            btc_price: 0,
-            status_text: "",
-            txn_fee: 0,
-            migration_alert: false,
-            migration_alert_text: '',
-            migration_complete: false,
-            fee: 0
-        };
+  constructor(props) {
+    super(props);
+    this.state = {
+      safex_key: this.props.data.safex_key,
+      loading: true,
+      address: "",
+      wif: "",
+      safex_bal: 0,
+      pending_safex_bal: 0,
+      btc_bal: 0,
+      pending_btc_bal: 0,
+      safex_price: 0,
+      btc_price: 0,
+      status_text: "",
+      txn_fee: 0,
+      migration_alert: false,
+      migration_alert_text: "",
+      migration_complete: false,
+      fee: 0,
+      confirm_migration: false
+    };
 
-        this.burnSafex = this.burnSafex.bind(this);
-        this.validateAmount = this.validateAmount.bind(this);
-        this.goBack = this.goBack.bind(this);
-        this.setOpenMigrationAlert = this.setOpenMigrationAlert.bind(this);
-        this.setCloseMigrationAlert = this.setCloseMigrationAlert.bind(this);
-        this.backToStep5 = this.backToStep5.bind(this);
-    }
+    this.burnSafex = this.burnSafex.bind(this);
+    this.validateAmount = this.validateAmount.bind(this);
+    this.goBack = this.goBack.bind(this);
+    this.setOpenMigrationAlert = this.setOpenMigrationAlert.bind(this);
+    this.setCloseMigrationAlert = this.setCloseMigrationAlert.bind(this);
+    this.backToStep5 = this.backToStep5.bind(this);
+    this.toggleConfirmMigration = this.toggleConfirmMigration.bind(this);
+  }
 
-    componentDidMount() {
+  componentDidMount() {
+    this.setState({
+      address: this.props.data.address,
+      wif: this.props.data.wif,
+      fee: this.props.data.fee,
+      safex_key: this.props.data.safex_key,
+      loading: false
+    });
+    this.getBalances(this.props.data.address);
+    this.getTxnFee();
+    this.setState({ loading: false });
+  }
+
+  getTxnFee() {
+    //public spend key is first half
+    get_utxos(this.props.data.address)
+      .then(utxos => {
+        const rawtx = generateSafexBtcTransaction(
+          utxos,
+          BURN_ADDRESS,
+          this.state.wif,
+          1,
+          this.props.data.fee
+        );
+        this.setState({ txn_fee: rawtx.fee / 100000000 });
+      })
+      .catch(err => console.log(err));
+  }
+
+  getBalances(address) {
+    var promises = [];
+    var json = {};
+    json["address"] = address;
+    promises.push(
+      fetch("http://omni.safex.io:3001/balance", {
+        method: "POST",
+        body: JSON.stringify(json)
+      })
+        .then(resp => resp.json())
+        .then(resp => {
+          return resp;
+        })
+    );
+    promises.push(
+      fetch(
+        "http://bitcoin.safex.io:3001/insight-api/addr/" + address + "/balance"
+      )
+        .then(resp => resp.text())
+        .then(resp => {
+          return resp;
+        })
+    );
+
+    Promise.all(promises)
+      .then(values => {
         this.setState({
-            address: this.props.data.address,
-            wif: this.props.data.wif,
-            fee: this.props.data.fee,
-            safex_key: this.props.data.safex_key,
-            loading: false
+          safex_bal: values[0].balance,
+          btc_bal: (values[1] / 100000000).toFixed(8),
+          safex_price: localStorage.getItem("safex_price"),
+          btc_price: localStorage.getItem("btc_price")
         });
-        this.getBalances(this.props.data.address);
-        this.getTxnFee();
-        this.setState({loading: false});
-    }
+      })
+      .catch(e => {
+        console.log(e);
+        this.setState({
+          status_text: "Sync error, please refresh"
+        });
+      });
+  }
 
-    getTxnFee() {
-        //public spend key is first half
-        get_utxos(this.props.data.address)
-            .then(utxos => {
-                const rawtx = generateSafexBtcTransaction(
-                    utxos,
-                    BURN_ADDRESS,
-                    this.state.wif,
-                    1,
-                    this.props.data.fee
-                );
-                this.setState({txn_fee: rawtx.fee / 100000000});
-            })
-            .catch(err => console.log(err));
-    }
-
-    getBalances(address) {
-        var promises = [];
-        var json = {};
-        json['address'] = address;
-        promises.push(fetch('http://omni.safex.io:3001/balance', {
-            method: "POST",
-            body: JSON.stringify(json)
-        }).then(resp => resp.json())
-            .then((resp) => {
-                return resp
-            }));
-        promises.push(fetch('http://bitcoin.safex.io:3001/insight-api/addr/' + address + '/balance')
-            .then(resp => resp.text())
-            .then((resp) => {
-                return resp
-            }));
-
-
-        Promise.all(promises).then(values => {
-            this.setState({
-                safex_bal: values[0].balance,
-                btc_bal: (values[1] / 100000000).toFixed(8),
-                safex_price: localStorage.getItem('safex_price'),
-                btc_price: localStorage.getItem('btc_price'),
-            });
-        }).catch(e => {
-            console.log(e)
-            this.setState({
-                status_text: 'Sync error, please refresh'
-            });
+  burnSafex(e) {
+    e.preventDefault();
+    const amount_value = document.getElementById("amount").value;
+    const amount = parseInt(amount_value);
+    if (amount === "") {
+      this.setOpenMigrationAlert("Please enter a valid amount");
+    } else {
+      this.setState({ loading: true });
+      get_utxos(this.state.address)
+        .then(utxos => {
+          const rawtx = generateSafexBtcTransaction(
+            utxos,
+            BURN_ADDRESS,
+            this.state.wif,
+            amount,
+            this.props.data.fee
+          );
+          return rawtx;
+        })
+        .then(rawtx => broadcastTransaction(rawtx))
+        .then(() => {
+          this.setState({
+            loading: false,
+            migration_complete: true
+          });
+          this.props.refresh();
+          this.toggleConfirmMigration();
+        })
+        .catch(err => {
+          console.log("error broadcasting transaction " + err);
+          this.setOpenMigrationAlert("error broadcasting transaction " + err);
         });
     }
+  }
 
-    burnSafex(e) {
-        e.preventDefault();
-        const amount = parseInt(e.target.amount.value);
-        if (e.target.amount.value === '') {
-            this.setOpenMigrationAlert("Please enter a valid amount");
-        } else {
-            this.setState({loading: true});
-            get_utxos(this.state.address)
-                .then(utxos => {
-                    const rawtx = generateSafexBtcTransaction(
-                        utxos,
-                        BURN_ADDRESS,
-                        this.state.wif,
-                        amount,
-                        this.props.data.fee
-                    );
-                    return rawtx;
-                })
-                .then(rawtx => broadcastTransaction(rawtx))
-                .then(() => {
-                    this.setState({
-                        loading: false,
-                        migration_complete: true,
-                    });
-                    this.props.refresh();
-                })
-                .catch(err => {
-                    console.log("error broadcasting transaction " + err);
-                    this.setOpenMigrationAlert("error broadcasting transaction " + err);
-                });
-        }
-
+  validateAmount(e) {
+    if (parseInt(e.target.value) > this.state.safex_bal) {
+      console.log(
+        "Not enough safex balance for that transaction, max is " +
+          this.state.safex_bal
+      );
+      this.setOpenMigrationAlert(
+        "Not enough safex balance for that transaction, max is" +
+          this.state.safex_bal
+      );
+      e.target.value = this.state.safex_bal;
     }
+  }
 
-    validateAmount(e) {
-        if (parseInt(e.target.value) > this.state.safex_bal) {
-            console.log("Not enough safex balance for that transaction, max is " + this.state.safex_bal);
-            this.setOpenMigrationAlert('Not enough safex balance for that transaction, max is' + this.state.safex_bal);
-            e.target.value = this.state.safex_bal;
-        }
-    }
+  goBack() {
+    this.props.setMigrationProgress(3);
+  }
 
-    goBack() {
-        this.props.setMigrationProgress(3);
-    }
+  setOpenMigrationAlert(message) {
+    openMigrationAlert(this, message);
+  }
 
-    setOpenMigrationAlert(message) {
-        openMigrationAlert(this, message);
-    }
+  setCloseMigrationAlert() {
+    closeMigrationAlert(this);
+  }
 
-    setCloseMigrationAlert() {
-        closeMigrationAlert(this);
-    }
+  backToStep5() {
+    this.setState({
+      migration_complete: false,
+      confirm_migration: false
+    });
+  }
 
-    backToStep5() {
-        this.setState({migration_complete: false});
-    }
+  toggleConfirmMigration(e) {
+    e.preventDefault();
+    this.setState({
+      confirm_migration: !this.state.confirm_migration
+    });
+  }
 
-    //create safex blockchain key set
-    render() {
-        return (
-            <div className="final-step">
-                {
-                    this.state.migration_complete
-                    ?
-                        <div>
-                            <p className="green-text">Migration of your tokens has started. This process may take a couple of days, please be patient while migration transaction is being processed.</p>
-                            <button className="button-shine" onClick={this.backToStep5}>Ok</button>
-                        </div>
-                    :
-                        <div>
-                            <p>Final Step</p>
-                            <p><span>You will need</span> {this.state.txn_fee} btc </p>
+  //create safex blockchain key set
+  render() {
+    return (
+      <div className="final-step">
+        {this.state.migration_complete ? (
+          <div>
+            <p className="green-text">
+              Migration of your tokens has started. This process may take a
+              couple of days, please be patient while migration transaction is
+              being processed.
+            </p>
+            <button className="button-shine" onClick={this.backToStep5}>
+              Ok
+            </button>
+          </div>
+        ) : (
+          <div>
+            <p>Final Step</p>
+            <p>
+              <span>You will need</span> {this.state.txn_fee} btc{" "}
+            </p>
 
-                            <form className="final-step-form" onSubmit={this.burnSafex}>
-                                <input onChange={this.validateAmount} name="amount" placeholder="Amount"/>
-                                <button className="button-shine">send</button>
-                            </form>
+            <form
+              className="final-step-form"
+              onSubmit={this.toggleConfirmMigration}
+            >
+              <input
+                onChange={this.validateAmount}
+                name="amount"
+                placeholder="Amount"
+                id="amount"
+              />
+              <button className="button-shine">send</button>
+            </form>
 
-                            <p>
-                                <span className="span-200">Your target migration address:</span><br />
-                                <input className="target-address" value={this.state.safex_key.public_addr} />
-                            </p>
-                            <p>
-                                <span className="span-200">The burn address:</span><br />
-                                <input className="target-address" value={BURN_ADDRESS} />
-                            </p>
+            <p>
+              <span className="span-200">Your target migration address:</span>
+              <br />
+              <input
+                className="target-address"
+                value={this.state.safex_key.public_addr}
+              />
+            </p>
+            <p>
+              <span className="span-200">The burn address:</span>
+              <br />
+              <input className="target-address" value={BURN_ADDRESS} />
+            </p>
 
-                            <MigrationAlert
-                                migrationAlert={this.state.migration_alert}
-                                migrationAlertText={this.state.migration_alert_text}
-                                closeMigrationAlert={this.setCloseMigrationAlert}
-                            />
-                        </div>
-                }
-            </div>
-        )
-    }
+            <MigrationAlert
+              migrationAlert={this.state.migration_alert}
+              migrationAlertText={this.state.migration_alert_text}
+              closeMigrationAlert={this.setCloseMigrationAlert}
+            />
+
+            <ConfirmMigration
+              confirmMigraton={this.state.confirm_migration}
+              burnSafex={this.burnSafex}
+              closeConfirmMigration={this.toggleConfirmMigration}
+            />
+          </div>
+        )}
+      </div>
+    );
+  }
 }
